e059281f34cf9529cf10634f534fa52f05a4af42
Santiago Muscolo <smuscolo2003@gmail.com>
2025-12-06 17:05:39 -0300

    flag-shader

--- shaders/src/script.js
+++ shaders/src/script.js
@@ -20,21 +20,40 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const flagTexture = textureLoader.load("./textures/flag-french.jpg");
 
 /**
  * Test mesh
  */
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+// const count = geometry.attributes.position.count;
+// const randoms = new Float32Array(count);
+
+// for(let i = 0; i < count; i++){
+//   randoms[i] = Math.random();
+// }
+
+// geometry.setAttribute('random', new THREE.BufferAttribute(randoms, 1));
 
 // Material
-const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
   vertexShader: vertexShader,
   fragmentShader: fragmentShader,
+  uniforms: {
+    uFrequency: { value: new THREE.Vector2(10.0, 5.0) }, // frequency of the sine wave
+    uTime: { value: 0 },
+    uColor: { value: new THREE.Color("orange") },
+    uTexture: { value: flagTexture },
+  }
 });
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('Frequency X');
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('Frequency Y');
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
+mesh.scale.y = 2 / 3;
 scene.add(mesh);
 
 /**
@@ -92,6 +111,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update material
+  material.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
