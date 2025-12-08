96947f976032622e98ee0867996e97b6e3513eb6
Santiago Muscolo <smuscolo2003@gmail.com>
2025-12-08 16:18:53 -0300

    animated-galaxy

---
+++ animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -0,0 +1,19 @@
+varying vec3 vColor;
+
+void main() {
+    //Diffuse point
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength *= 2.0;
+    // strength = 1.0 - strength;
+
+    //Light point
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = 1.0 - strength;
+    strength = pow(strength, 10.0);
+
+    //Color
+    vec3 color = mix(vec3(0.0), vColor, strength);
+
+    gl_FragColor = vec4(vec3(color), 1.0);
+    #include <colorspace_fragment>
+}
